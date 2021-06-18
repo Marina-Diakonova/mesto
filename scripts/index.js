7e4163e7719ac17dbcd4bfe196add6f00a70c369
Marina Diakonova <83052113+Marina-Diakonova@users.noreply.github.com>
2021-06-18 09:11:34 +0500

    style: внесены поправки

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,16 @@ let callEditProfileButton = document.querySelector('.profile__edit')
 let callPopup = document.querySelector('.popup');
 let callPopupClose = document.querySelector('.popup__close')
 let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__item_name')
-let jobInput = document.querySelector('.popup__item_description')
-let inputsNamePopup = document.querySelector("input[id='popup__name']")
-let inputsDescriptionPopup = document.querySelector("input[id='popup__description']")
+let nameInput = document.querySelector('.popup__item-name')
+let jobInput = document.querySelector('.popup__item-description')
 let profileName = document.querySelector('.profile__name')
-let profileDescription = document.querySelector('.profile__description') 
+let profileDescription = document.querySelector('.profile__description')
 
 //------------------------код вызова карточки POPUP и подмена полей из профиля-------------------
 let showPopupCard = function () {
     callPopup.classList.add('popup_opened');
-    inputsNamePopup.value = profileName.textContent;
-    inputsDescriptionPopup.value = profileDescription.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
 }
 
 //--------------------------код закрытия карточки POPUP-----------------------------------------
@@ -25,8 +23,8 @@ let closePopupCard = function () {
 //---------------------------функция сохранения данных PROFILE-----------------------------------
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    profileName.textContent = inputsNamePopup.value;
-    profileDescription.textContent = inputsDescriptionPopup.value;
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
     closePopupCard();//---закрываем карточку вызвав функцию закрытия прописанную ранее-----------
 }
 
